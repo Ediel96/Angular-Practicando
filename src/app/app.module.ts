c94acbb0ad19c70b1f7ae552c8ee8cf78312e794
DarkEdiel96 <h.amiltone.diel@gmail.com>
2019-07-07 19:37:25 -0500

    configurando las carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BuscadorComponent,
+    HeroesComponent,
+    HeroeTarjetaComponent,
+    HeroeComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
